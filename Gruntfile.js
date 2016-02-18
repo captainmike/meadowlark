e2bb15b5bbef9c395d75364d0a69f3b2a0adc776
Captain Mike <mike@captain-refactor.co.za>
2016-02-18 15:46:12 +0200

    Set-up Grunt to clean and copy assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,57 @@ module.exports = function(grunt){
 
   // configure plugins
   grunt.initConfig({
+    jshint: {
+      options: {esversion: 6},
+      app: ['lib/**/*.js'],
+      tests: ['specs/**/*_specs.js']
+    },
+
+    mochacli: {
+      all: ['specs/**/*_specs.js']
+    },
+
+    clean: {
+      build: {
+        src: ['public/']
+      }
+    },
+
     sass: {
       options: {
-        sourceMap: true,
-        outputStyle: 'expanded',
-        includePaths: ['node_modules/bootstrap/scss']
+        includePaths: ['node_modules/bootstrap/scss'],
       },
+
+      dev: {
+        options: {
+          sourceMap: true,
+          outputStyle: 'expanded',
+        },
+        files: {
+          'public/app.css': 'assets/stylesheets/main.scss'
+        }
+      },
+
       dist: {
+        options: {
+          sourceMap: false,
+          outputStyle: 'compressed',
+        },
         files: {
           'public/app.css': 'assets/stylesheets/main.scss'
         }
       }
     },
-    mochacli: {
-      all: ['specs/**/*_specs.js']
-    },
-    jshint: {
-      options: {esversion: 6},
-      app: ['lib/**/*.js'],
-      tests: ['specs/**/*_specs.js']
+
+    copy: {
+      dist: {
+        files: [{expand: true, cwd: 'assets/images/', src: '*', dest: 'public/images/'}]
+      }
     }
   });
 
   // register tasks
   grunt.registerTask('default', ['mochacli', 'jshint']);
+  grunt.registerTask('build:dev', ['clean', 'copy:dist', 'sass:dev']);
+  grunt.registerTask('build:prod', ['clean', 'copy:dist', 'sass:dist']);
 };
